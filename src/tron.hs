7551bbf265418aafc0feecaca80a53a3455bcf2b
omerzk <mine!>
2015-06-25 12:48:03 +0300

    still need to CleanUp

--- src/tron.hs
+++ src/tron.hs
@@ -6,9 +6,11 @@ import qualified FRP.Helm.Keyboard as Keyboard
 import qualified FRP.Helm.Time as Time
 import qualified FRP.Helm.Text as Text
 
+
 data PlayerState = PlayerState {coordinates::[(Double,Double)], color :: Color, alive :: Bool, prevX::Double, prevY::Double, inptInd::Int}
 data GameState = GameState {state::Bool, players::[PlayerState]}
-type Input = [(Int,Int)]--TODO: this is where single vs multi comes into play
+type Input = [(Int, Int)]
+
 speed::Double
 speed = 4
 
@@ -30,7 +32,7 @@ PlayerState {coordinates = [(20,0)],
          prevY = 0,
           inptInd = 1}]}
 
-
+-- The response funcion which creates a new game state based on the input signal thus continuing the game signal
 update:: Input -> GameState -> GameState
 update input game =
   if state game then GameState {state = (fst (input !! 2) == 0), players = players'} else if snd (input !! 2) == 1 then defaultGame else GameState {state = (input !! 3) == (-1, -1), players = players game} -- redundent and ugly
@@ -51,6 +53,7 @@ updatePlayer players (dx, dy) state
                   alive' = not $ crossed (px',py')
 
                   crossed (x,y) = (any (\pState -> (x,y) `elem` (coordinates pState)) players)
+
                   detMove dw dz prev
                     |(dz == 0) && dw/= 0 && (realToFrac (signum dw) + signum(prev) /= 0) = (realToFrac dw) * speed
                     |(dz /= 0) && dw == 0 = 0
@@ -76,19 +79,23 @@ render :: GameState  -> (Int, Int) -> Element
 render game (w, h)  =
   if state game then centeredCollage w h (back:(concatMap playerForm (players game))) else centeredCollage w h pauseScreen  -- ++ others-- ++ $map shotForm survivors --(\pState -> playerForm pState) survivors--do i even need the lambda?
     where back1 = gradient grad $ square 750
-back = group [back1, rotate 1.571 back1] --  move(-380,-380) (toForm (image w h "blueback.png"))
-grad = radial (300, 300) 20 (0, 0) 20 [(0, teal), (1, navy)]
-pauseScreen = [im, im3, (gradient (linear (0, 0) (100, 100) [(0, maroon), (1, teal)]) $ ngon 8 200), move (0, -125) (filled red $ oval 50 30), rotate 1 (move (70, 50) eye), rotate 2.3 (move (-70, 50) eye), mouth]
-txt = blank--move(0,300) $ toForm $ Text.text $Text.toText $unlines ["Space - Start/Pause"," "," ","Tab -Reset"] -- Text{textUTF8 = "Start = Down+Right, Reset = Tab, Pause = space", textColor = lime} -- , fontSize = 30, fontWeight =10, fontSlant = FontSlantOblique
-im = move(-400, -350) (toForm (image w h "tri.png"))
+          back = group [back1, rotate 1.571 back1] --  move(-380,-380) (toForm (image w h "blueback.png"))
+          grad = radial (300, 300) 20 (0, 0) 20 [(0, blue), (1, navy)]
+          pauseScreen = [im, im3, (gradient (linear (0, 0) (100, 100) [(0, maroon), (1, teal)]) $ ngon 8 200), move (0, -125) (filled red $ oval 50 30), eyer, eyel, mouth, txt]
+          txt =blank --move(0,300) $ toForm $ Text.text $Text.toText $unlines ["Space - Start/Pause"," "," ","Tab -Reset"] -- Text{textUTF8 = "Start = Down+Right, Reset = Tab, Pause = space", textColor = lime} -- , fontSize = 30, fontWeight =10, fontSlant = FontSlantOblique
+          im = move(-400, -350) (toForm (image w h "tri.png"))
 --make these blanks to remove pause face
-mouth = move(-40, 80) (toForm (image 100 100 "mouth.png"))
-im3 = move(-375, -175) (toForm (image 750 750 "bear.png"))-- TODO: decide to save or not
-          eye = (filled white $oval 35 80)
+  mouth = move(-40, 80) (toForm (image 100 100 "mouth.png"))
+          im3 = blank -- move(-375, -175) (toForm (image 750 750 "bear.png"))
+          eyer = move(25, -10) (toForm (image 80 80 "eyer.png"))
+          eyel = move(-110, -10) (toForm (image 80 80 "eyes.png"))
 -- i guess it's kinda stupid to compose a screen of several overlaying images... consider saving the image of the head/whole pause screen and rendering it as a single
--- survivors = filter (\pState -> alive pState) (players game) endScreen
+--survivors = filter (\pState -> alive pState) (players game) endScreen
+--eye = (filled white $oval 35 80)
+--rotate 1 (move (70, 50) eye), rotate 2.3 (move (-70, 50) eye)
 
-
+-- the game signal the signal which encapsulates every other signal in the game,
+--  including a time signal to set a sample rate an input signal from obviuos reasons and a games signal which constructs all frames of th egame
 gameSignal :: FRP.Helm.Signal GameState
 gameSignal = foldp update defaultGame input
   where input = lift2 processKeys Keyboard.keysDown (Time.fps 40)
@@ -106,7 +113,6 @@ gameSignal = foldp update defaultGame input
               |key2 `elem` keys = 1
               |otherwise = 0
 
--- TODO: this is KindaCrap,
 -- instead of saving every (x,y) save every nth pair(add a counter and  a prevP = (x,y)(so the next coord would be correct) )
 --  and for the other use a last coordinate field, where in the form func chain the field to the coordinates
 --  THIS WOULD ELIMINATE THE WIGGLING MOTIONS--------------------------------------------------------------------------------
